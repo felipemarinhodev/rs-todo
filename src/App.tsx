115108b96ea060e7adff996710952a87bb29ad22
Felipe Marinho <github@felipemarinho.dev>
2024-12-20 08:08:51 -0800

    create a header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import styles from "./App.module.css";
 import "./global.css";
+
+import logo from "./assets/Logo.svg";
 
 function App() {
 
   return (
     <>
+      <header className={styles.header}>
+        <img
+          src={logo}
+          alt="logo todo"
+        />
+      </header>
       <p>
         Todo
       </p>
